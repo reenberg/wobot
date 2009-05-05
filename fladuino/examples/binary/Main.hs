1ad481af353c8ed54ef9b4562682277b7802fe75
Troels Henriksen <athas@lambda.athas.dyndns.dk>
2009-05-05 16:03:52 +0200

    Super elegance for binary counter example!

--- fladuino/examples/binary/Main.hs
+++ fladuino/examples/binary/Main.hs
@@ -22,39 +22,35 @@ import Fladuino.Devices
 main :: IO ()
 main =
     defaultMain $ do
-    addCInclude "math.h"
     addCImport "isBitSet" [$ty|(Integer, Integer) -> Bool|] [$cexp|isBitSet|]
     addCFundef [$cedecl|int isBitSet(int x, int bit) {
                                   return (x & (1 << bit));
                                 }|]
-    genStreams ((map unS . concat) $ map (\n -> [clocked >>> maybeTurnOn n, clocked >>> maybeTurnOff n]) [0..3])
-        where
-          modNum :: S Integer -> S Integer
-          modNum = sintegrate zero int
-              where
-                zero :: N Integer
-                zero = liftN [$exp|0|]
-
-                int :: N ((Integer, Integer) -> (Integer, Integer))
-                int = liftN [$decls|f (n, x) = (x + n, x + n)|]
-
-          ifBitOn :: Integer -> S Integer -> S Integer
-          ifBitOn n = sfilter [$exp|\p -> isBitSet(p, $int:n)|]
-
-          ifBitOff :: Integer -> S Integer -> S Integer
-          ifBitOff n = sfilter [$exp|\p -> not (isBitSet(p, $int:n))|]
-
-          maybeTurnOn :: Integer -> S Integer -> S ()
-          maybeTurnOn n from = from >>> ifBitOn n >>> turnOn (diode (5+n) False)
-
-          maybeTurnOff :: Integer -> S Integer -> S ()
-          maybeTurnOff n from = from >>> ifBitOff n >>> turnOff (diode (5+n) False)
-
-          displayBit :: Integer -> S Integer -> S ()
-          displayBit n from = from >>> (maybeTurnOn n)
-
-          s1 = onEvent (PushButtonPressEvent $ PushButton 2) >>> sconst [$exp|1|]
-          s2 = onEvent (PushButtonReleaseEvent $ PushButton 3) >>> sconst [$exp|-1|]
-          s = smerge s1 s2
-          clocked = smerge s (clock 20 >>> sconst [$exp|1|]) >>> modNum
+    genStreams $ map unS (streamsForCounter 8)
+        
           
+streamsForCounter :: Integer -> [S ()]
+streamsForCounter n = concat $ map (\n -> [clocked >>> maybeTurnOn n, clocked >>> maybeTurnOff n]) [0..n-1]
+    where
+      modNum :: S Integer -> S Integer
+      modNum = sintegrate zero int
+          where
+            zero :: N Integer
+            zero = liftN [$exp|0|]
+
+            int :: N ((Integer, Integer) -> (Integer, Integer))
+            int = liftN [$decls|f (n, x) = (x + n, x + n)|]
+
+      bitStatus :: Integer -> S Integer -> S Bool
+      bitStatus i = smap [$exp|\p -> isBitSet(p, $int:i)|]
+
+      maybeTurnOn :: Integer -> S Integer -> S ()
+      maybeTurnOn i from = from >>> bitStatus i >>> sfilter [$exp|id|] >>> turnOn (diode (n+1+i) False)
+
+      maybeTurnOff :: Integer -> S Integer -> S ()
+      maybeTurnOff i from = from >>> bitStatus i >>> sfilter [$exp|(\v -> not v)|] >>> turnOff (diode (n+1+i) False)
+
+      s1 = onEvent (PushButtonPressEvent $ PushButton 2) >>> sconst [$exp|1|]
+      s2 = onEvent (PushButtonReleaseEvent $ PushButton 3) >>> sconst [$exp|-1|]
+      s = smerge s1 s2
+      clocked = smerge s (clock 20 >>> sconst [$exp|1|]) >>> modNum
